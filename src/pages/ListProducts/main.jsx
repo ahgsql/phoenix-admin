849a111c3f7c01ef551d0ce669673c94e5bca96b
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-09-02 19:48:41 +0300

    products list

--- src/pages/ListProducts/main.jsx
+++ src/pages/ListProducts/main.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import getProducts from "../../hooks/getProducts";
+import { Content } from "antd/es/layout/layout";
+import { Table } from "antd";
 
 export default function ListProducts() {
-  return <div>ListProducts</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      let data = await getProducts();
+      setProducts(data);
+    })();
+  }, []);
+
+  const columns = [
+    {
+      title: "Product Title",
+      dataIndex: "productTitle",
+      key: "productTitle",
+    },
+    {
+      title: "Product Price",
+      dataIndex: "productPrice",
+      key: "productPrice",
+    },
+    {
+      title: "Slug",
+      dataIndex: "productSlug",
+      key: "productSlug",
+    },
+  ];
+
+  if (products.length == 0) {
+    return <p>Loading..</p>;
+  }
+
+  return (
+    <Content style={{ padding: "0 24px", minHeight: 280 }}>
+      <Table dataSource={products} rowKey={"_id"} columns={columns} />;
+    </Content>
+  );
 }
