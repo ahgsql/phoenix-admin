210cb6331b09b1f9ebaa3e3a1ee0b2b4b3590018
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-09-07 14:06:41 +0300

    add product page

--- src/pages/AddProduct/main.jsx
+++ src/pages/AddProduct/main.jsx
@@ -1,11 +1,85 @@
 import React from "react";
-
-import { Breadcrumb, Layout, Menu, theme } from "antd";
-
-const { Header, Content, Footer, Sider } = Layout;
+import { Button, Form, Input, InputNumber } from "antd";
+import addProduct from "../../hooks/addProduct";
 
 export default function AddProduct() {
+  const [form] = Form.useForm();
+  const onFinish = () => {
+    let formData = form.getFieldsValue();
+    let data = {
+      productTitle: formData.title,
+      productPrice: formData.price,
+      productSlug: formData.slug,
+      productImg: [{ url: formData.productImg }],
+    };
+    addProduct(data);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
   return (
-    <Content style={{ padding: "0 24px", minHeight: 280 }}>Add Product</Content>
+    <Form
+      form={form}
+      name="control-hooks"
+      onFinish={onFinish}
+      style={{
+        maxWidth: 600,
+      }}
+    >
+      <Form.Item
+        name="title"
+        label="Product Title"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="price"
+        label="Product Price"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <InputNumber />
+      </Form.Item>
+      <Form.Item
+        name="slug"
+        label="Product Slug"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="productImg"
+        label="Image Url"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
