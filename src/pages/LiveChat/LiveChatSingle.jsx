d3375efef1b926716ed41864e1d161ff5c4fd358
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-09-14 15:19:07 +0200

    livechat changes

--- src/pages/LiveChat/LiveChatSingle.jsx
+++ src/pages/LiveChat/LiveChatSingle.jsx
@@ -4,41 +4,34 @@ import axios from "axios";
 import Pusher from "pusher-js";
 import { useParams } from "react-router";
 import getChatHistory from "../../hooks/getChatHistory";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import ChatList from "./ChatList";
 export default function LiveChatSingle() {
   const [history, setHistory] = useState([]);
   const [message, setMessage] = useState("");
   const { userName } = useParams();
-
-  let bind = false;
-  let historyGot = false;
   useEffect(() => {
     const pusher = new Pusher("cc9492824097dabab1e1", {
       cluster: "eu",
     });
     const channel = pusher.subscribe("liveChat");
-    if (!bind) {
-      channel.bind("customer-message-from-" + userName, function (data) {
-        setHistory((prevhistory) => [
-          ...prevhistory,
-          { from: "customer", message: data.message },
-        ]);
-      });
-      bind = true;
-    }
 
-    (async () => {
-      if (historyGot) return;
-      let history = await getChatHistory(userName);
-      history.map((message) => {
-        setHistory((prevhistory) => [
-          ...prevhistory,
-          { from: message.from, message: message.message },
-        ]);
-      });
-      historyGot = true;
-    })();
-  }, []);
+    channel.bind("customer-message-from-" + userName, function (data) {
+      setHistory((prevhistory) => [
+        ...prevhistory,
+        { from: "customer", message: data.message },
+      ]);
+    });
 
+  }, [userName]);
+
+  useEffect(() => {
+
+    getChatHistory(userName).then(setHistory)
+
+  }, [userName]);
+  
   const sendMessage = async () => {
     if (message.trim().length > 0) {
       let response = await axios.post(
@@ -57,31 +50,43 @@ export default function LiveChatSingle() {
   };
   return (
     <>
-      <div className="chat-area">
-        <div className="chat-history">
-          {history.map((message, i) => {
-            return (
-              <div className={message.from + "-msg message"} key={i}>
-                {" "}
-                <div className="messageLabel">
-                  {message.from == "admin" ? "You" : "Client"}:
+      <div className="chatAndList" style={{ display: "flex", flexDirection: "row", gap: 50 }}>
+        <div className="chat-area">
+          <div className="chat-history">
+            {history.map((message, i) => {
+              return (
+                <div className={message.from + "-msg message"} key={i}>
+                  {" "}
+                  <div className="messageLabel">
+                    {message.from == "admin" ? "You" : userName}:
+                  </div>
+                  <div className="messageBody">{message.message}</div>
                 </div>
-                <div className="messageBody">{message.message}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <div className="chat-input" style={{
+            display: "flex",
+            flexDirection: "row",
+            padding: "10px",
+            alignContent: "baseline",
+          }}>
+            <input
+              type="text"
+              className="message-input"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <button className="send-button" onClick={sendMessage}><FontAwesomeIcon icon={faPaperPlane} size="xl" style={{ color: "#658fd7", }} /></button>
+          </div>
+
         </div>
-        <div className="chat-input">
-          <input
-            type="text"
-            className="message-input"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button onClick={sendMessage}>send</button>
-        </div>
+
+        <ChatList />
+
       </div>
+
     </>
   );
 }
