7b0a92615e62c6f3779fe2360d3eda581f726f98
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-09-14 21:45:14 +0300

    Live Chat Fixes

--- src/pages/LiveChat/ChatList.jsx
+++ src/pages/LiveChat/ChatList.jsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import getChats from "../../hooks/getChats";
 import { Skeleton } from "antd";
-export default function LiveChat() {
+export default function ChatList() {
   const [list, setChatList] = useState([]);
   const [loading, setLoading] = useState(true);
   let bind = false;
@@ -22,17 +22,20 @@ export default function LiveChat() {
     })();
   }, []);
   if (loading) return <h1>Loading</h1>;
+
   return (
     <>
       <div className="chat-list">
         {list.map((chat) => {
           return (
-            <Link to={"/chat/" + chat.userName}>
-              <div className="singleChat">
-                <span style={{ fontWeight: "bold" }}>{chat.userName}</span>
-                <p>Total message count: {chat.messageCount}</p>
-              </div>
-            </Link>
+            <>
+              <Link to={"/chat/" + chat.userName}>
+                <div className="singleChat">
+                  <span style={{ fontWeight: "bold" }}>{chat.userName}</span>
+                  <p>Total message count: {chat.messageCount}</p>
+                </div>
+              </Link>
+            </>
           );
         })}
       </div>
